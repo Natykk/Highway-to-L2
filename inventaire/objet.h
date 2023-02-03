36979400dbb18f4997a5dd4ca40456548ce68f5d
Matéo GALLAIS <mateo.gallais@tutanota.com>
2023-02-03 16:09:39 +0100

    Correction objet.h

--- inventaire/objet.h
+++ inventaire/objet.h
@@ -47,75 +47,71 @@ objet_t tab_objet[NB_OBJET] = {
     {
         "Or",
         "Echange le avec un marchant tu vas voir c'est pratique",
-    }
+    },
     {
         "Green Goo",
         "Goute c'est bon",
-    }
+    },
     {
         "Fourrure",
         "Ca en ferait un tres bon tapis"
-    }
+    },
     {
         "Griffe",
         "A grou grou"
-    }
+    },
     {
         "Grimoire",
         "Trop chiant a lire va voir le marchant"
-    }
+    },
     {
         "Grey Goo",
         "Avale c'est pas mauvais"
-    }
+    },
     {
         "Pioche",
         "I AM A DWARF AND I'M DIGGING A HOLE"
-    }
+    },
     {
         "Lance-Pierre",
         "Rah ce nain"
-    }
+    },
     {
         "Cailloux",
         "Bah ouais..."
-    }
+    },
     {
         "Massue",
         "Ne pose pas ca sur n'importe quelle tete"
-    }
+    },
     {
         "Red Goo",
         "Delecte toi c'est appreciable"
-    }
+    },
     {
         "Cornes de Diablotin",
         "C'est deux longs objets, souvent associés en paire, sont maintenu sur le haut du crane d'un petit diablotin. Vous remarquerez sans doutes des similarités avec celle d'une chevre."
-    }
+    },
     {
         "Queue de Diablotin",
         "Un diablotin est, dans la mythologie, le folklore et plusieurs traditions, une créature démoniaque de petite taille. Ils sont décrits comme de petites êtres espiègles agités et de taille réduite plutôt que de sérieuses menaces, et occupent une position inférieure dans la hiérarchie de la démonologie. Les assistants du Diable sont parfois nommés des « diablotins »."
-    }
+    },
     {
         "Lance",
         "Compliqué a ranger dans un sac"
-    }
+    },
     {
         "Coeur de Diable",
         "Mais pourquoi... Mais c'est immonde..."
-    }
+    },
     {
         "Tetes",
         "Ah nan là c'est trop pour moi"
-    }
+    },
     {
         "Croquettes",
         "Vraiment ?!"
     }
-    /**
-     * @brief a remplir
-     * 
-     */
 }
 
 void afficher_objet(objet_inv_t *);
