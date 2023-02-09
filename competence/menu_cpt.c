6d198ea09384261cbf42b1c7ea4cc158c64fb732
Nathan <nat.gauteron@gmail.com>
2023-02-09 16:07:26 +0100

    Ajout Page sur la page de compétence

--- competence/menu_cpt.c
+++ competence/menu_cpt.c
@@ -26,16 +26,22 @@ Button button4;
 
 // Initialisation de l'image de fond
 SDL_Surface* background = NULL;
+SDL_Surface* background2 = NULL;
 
 void exit_all(){
     SDL_FreeSurface(background);
+    SDL_FreeSurface(background2);
     SDL_FreeSurface(button1.surface);
     SDL_FreeSurface(button2.surface);
+    SDL_FreeSurface(button3.surface);
+    SDL_FreeSurface(button4.surface);
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
 int main() {
+  int larg;
+  int longueur;
   // Initialisation de la SDL
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("Erreur SDL Init %s\n", SDL_GetError());
@@ -48,7 +54,7 @@ int main() {
     // ajout du texte TTF 
     TTF_Init();
     TTF_Font *police = NULL;
-    SDL_Color couleurNoire = {0, 0, 0};
+    SDL_Color couleurNoire = {255, 255, 255};
     police = TTF_OpenFont("../font/necrosans.ttf", 65);
     
     
@@ -73,28 +79,33 @@ int main() {
     printf("Unable to load image %s! SDL Error: %s\n", "background", SDL_GetError());
     return 1;
   }
+  background2 = IMG_Load("background_comp.jpg");
+  if (background2 == NULL) {
+    printf("Unable to load image %s! SDL Error: %s\n", "background", SDL_GetError());
+    return 1;
+  }
   
   // Initialisation des boutons
-  button1.x = WINDOW_WIDTH  / 2 - (WINDOW_WIDTH  / 5);
-  button1.y = WINDOW_HEIGHT / 2;
+  button1.x = 250;
+  button1.y = 100;
   button1.w = 100*3;
   button1.h = 50*3;
   button1.surface = TTF_RenderText_Blended(police, "Comp1", couleurNoire);
   
-  button2.x = button1.x + button1.w + 100; 
-  button2.y = WINDOW_HEIGHT / 3 ;
+  button2.x = button1.x + 400; 
+  button2.y = button1.y;
   button2.w = 100*3;
   button2.h = 50*3;
   button2.surface = TTF_RenderText_Blended(police, "Comp2", couleurNoire);
 
-  button3.x = button1.x + button1.w + 100; 
-  button3.y = WINDOW_HEIGHT / 4 ;
+  button3.x = button2.x + 400; 
+  button3.y = button2.y;
   button3.w = 100*3;
   button3.h = 50*3;
   button3.surface = TTF_RenderText_Blended(police, "Comp3", couleurNoire);
   
-  button4.x = button1.x + button1.w + 100; 
-  button4.y = WINDOW_HEIGHT / 5 ;
+  button4.x = button3.x + 400; 
+  button4.y = button2.y;
   button4.w = 100*3;
   button4.h = 50*3;
   button4.surface = TTF_RenderText_Blended(police, "Comp4", couleurNoire);
@@ -127,8 +138,14 @@ int main() {
     }
   }
 }
-
+SDL_Rect backgroundRect ;
 // Collage de l'image de fond sur la surface de l'écran
+/*for(larg=0,longueur=0;larg>WINDOW_WIDTH && longueur>WINDOW_HEIGHT ;larg+=WINDOW_WIDTH/512,longueur+=WINDOW_HEIGHT/512){
+  SDL_BlitSurface(background,NULL,screenSurface,NULL);
+}*/
+backgroundRect.x=1024;
+backgroundRect.y=0;
+SDL_BlitSurface(background2,NULL,screenSurface,&backgroundRect);
 SDL_BlitSurface(background, NULL, screenSurface, NULL);
 
 // Collage des boutons sur la surface de l'écran
@@ -137,6 +154,12 @@ SDL_BlitSurface(button1.surface, NULL, screenSurface, &buttonRect1);
 
 SDL_Rect buttonRect2 = { button2.x, button2.y, button2.w, button2.h };
 SDL_BlitSurface(button2.surface, NULL, screenSurface, &buttonRect2);
+
+SDL_Rect buttonRect3 = { button3.x, button3.y, button3.w, button3.h };
+SDL_BlitSurface(button3.surface, NULL, screenSurface, &buttonRect3);
+
+SDL_Rect buttonRect4 = { button4.x, button4.y, button4.w, button4.h };
+SDL_BlitSurface(button4.surface, NULL, screenSurface, &buttonRect4);
 
 // Mise à jour de la fenêtre
 SDL_UpdateWindowSurface(window);
