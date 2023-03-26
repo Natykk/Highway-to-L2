cabe2354ecf55e7a1894d74078253e53b13f40e2
Theo-dlrd <Theo.Duluard.Etu@univ-lemans.fr>
2023-03-26 19:29:24 +0200

    que les autres ?

--- head/arbre.h
+++ head/arbre.h
@@ -12,5 +12,7 @@ int init_arbre(t_arbre ** arbre, t_competence *competences, t_classe typeClasse)
 void aff_classe(t_arbre * arbre);
 int detruire_arbre(t_arbre ** arbre);
 int one_preced_cpt_debloq(t_competence * competence);
+int peut_deploquer_cpt(entite_t * personnage, t_competence * competence);
+int competence_debloquer(entite_t * personnage, t_competence * competence);
 
 #endif
