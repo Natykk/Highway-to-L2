9dfdc1a7142f367f6d7fba96cf6a1f04531a1e38
Theo-dlrd <Theo.Duluard.Etu@univ-lemans.fr>
2023-03-27 23:54:13 +0200

    ras

--- head/arbre.h
+++ head/arbre.h
@@ -14,6 +14,7 @@ int init_arbre(t_arbre ** arbre, t_competence *competences, t_classe typeClasse)
 void aff_classe(t_arbre * arbre);
 int detruire_arbre(t_arbre ** arbre);
 int one_preced_cpt_debloq(t_competence * competence);
+int cpt_in_arbre_joueur(entite_t * personnage, t_competence * competence);
 int peut_deploquer_cpt(entite_t * personnage, t_competence * competence, t_arbre * arbre_cpt);
 int competence_debloquer(entite_t * personnage, t_competence * competence, t_arbre * arbre_cpt);
 
